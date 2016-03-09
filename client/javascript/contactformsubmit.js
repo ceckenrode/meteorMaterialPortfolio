b3f2c2bcacf72adad1f46fc86d5fda37494f328c
ceckenrode <chriseckenrode@me.com>
2016-03-09 17:12:53 -0500

    set up google forms contact

--- client/javascript/contactformsubmit.js
+++ client/javascript/contactformsubmit.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
+  
+
+
+$(document).on('click', '#submitForm', function(event) {
+  event.preventDefault();
   var name = $('#name').val();
   var email = $('#email').val();
   var subject = $('#subject').val();
   var message = $('#message').val();
-  
+
+  $.ajax({
+  type: "POST",
+  crossDomain: true,
+  // dataType: 'jsonp',
+  url: "https://docs.google.com/forms/d/1XtkLm1rJhkeXFcmN5-3pbFq0WQpv58C7N9TMHunwHUE/formResponse",
+  data: {"entry.1405079923": name, "entry.10097707": email, "entry.1695378437": subject, "entry.1193375964": message}
+})
+
+  $('#name').val('');
+  $('#email').val('');
+  $('#subject').val('');
+  $('#message').val('');
+
+});
+ 
+
 });
