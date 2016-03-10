f8878033e7dd2977c339a345b3a1bc78940f5d6e
ceckenrode <chriseckenrode@me.com>
2016-03-10 11:13:07 -0500

    aligned bottom navigation button, added data scoll to top of page

--- client/javascript/contactformsubmit.js
+++ client/javascript/contactformsubmit.js
@@ -12,7 +12,7 @@ $(document).on('click', '#submitForm', function(event) {
   $.ajax({
   type: "POST",
   crossDomain: true,
-  // dataType: 'jsonp',
+  dataType: 'jsonp',
   url: "https://docs.google.com/forms/d/1XtkLm1rJhkeXFcmN5-3pbFq0WQpv58C7N9TMHunwHUE/formResponse",
   data: {"entry.1405079923": name, "entry.10097707": email, "entry.1695378437": subject, "entry.1193375964": message}
 })
